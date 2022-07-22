28738f1e9b764ccac4b342322a04d261dcf81d24
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2022-07-22 15:51:20 -0300

    Added Ejs integration

--- Lets Code/JULHO (NODE)/server.js
+++ Lets Code/JULHO (NODE)/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+
+
 const router = require("./userRoutes");
 const app = express();
 const port = 2001;
 
+app.set("view engine", "ejs");
 app.use("/", router);
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
--- Lets Code/JULHO (NODE)/userRoutes.js
+++ Lets Code/JULHO (NODE)/userRoutes.js
@@ -4,6 +4,11 @@ const axios = require("axios");
 const BASE_URL = "https://swapi.dev/api";
 const router = express.Router();
 
+async function getData(url){
+  let response = await axios.get(url);
+  return response.data
+}
+
 router.get("/people/:id", (req, res) => {
   let id = req.params.id;
   axios.get(BASE_URL + "/people/" + id).then((response) => {
@@ -17,6 +22,29 @@ router.get("/films/:id", (req, res) => {
     res.json(response.data);
   });
 });
+
+
+router.get("/", async (req,res) => {
+  let startshipsData = await getData(BASE_URL+"/starships/3")
+  let startshipsMovies = startshipsData.films
+  // let startshipName = startshipsData.name
+  let movies = []
+  async function getFilmsNames(item){
+    let info = await getData(item)
+    movies.push(info.title)
+    if(movies.length == startshipsMovies.length){
+      res.render("pages/index", {data:startshipsData, films:movies})
+
+    }
+  }
+  startshipsMovies.forEach(getFilmsNames)
+
+})
+
+
+router.get("/about", (req,res) => {
+  res.render("pages/about")
+})
 
 router.get("/starships/:id", (req, res) => {
   let id = req.params.id;
